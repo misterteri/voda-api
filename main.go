6dcd4f17857e2f7116787e7ea3891099ddcf3447
Bijon Setyawan Raya <nathan.setyawan96@gmail.com>
2022-05-09 08:45:28 +0800

    Create mux and graceful shutdown

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+	h "voda-api/handlers"
+)
+
+func main() {
+	logger := log.New(os.Stdout, "vodascheduler ", log.LstdFlags)
+
+	serveMux := http.NewServeMux()
+	serveMux.Handle("/post", h.NewPost(logger))
+	serveMux.Handle("/get", h.NewGet(logger))
+	serveMux.Handle("/delete", h.NewDelete(logger))
+
+	server := &http.Server{
+		Handler:      serveMux,
+		Addr:         ":8080",
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
+			logger.Fatal(err)
+		}
+	}() // run in background
+
+	signalChannel := make(chan os.Signal, 1)
+	signal.Notify(signalChannel, os.Interrupt, syscall.SIGTERM)
+
+	signal := <-signalChannel
+	logger.Println("Received termination, gracefully shutdown", signal)
+
+	tc, _ := context.WithDeadline(context.Background(), time.Now().Add(30*time.Second))
+	server.Shutdown(tc)
+}
